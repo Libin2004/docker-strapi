c068fa778cf4244ac554308b068a9cc064cd863e
Libin2004 <libinlibinlibin10@gmail.com>
2026-02-18 10:46:42 +0000

    single pipeline

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -5,8 +5,9 @@ resource "aws_ecs_task_definition" "strapi" {
   cpu                      = "512"
   memory                   = "1024"
 
-  execution_role_arn = var.ecs_task_role_arn
-  task_role_arn      = var.ecs_task_role_arn
+execution_role_arn = "arn:aws:iam::811738710312:role/ecs_fargate_taskRole"
+task_role_arn      = "arn:aws:iam::811738710312:role/ecs_fargate_taskRole"
+
 
   container_definitions = jsonencode([
     {
