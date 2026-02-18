15e849b2b4dbff0fa344eb6a80fcb576ebe8f6ae
Libin2004 <libinlibinlibin10@gmail.com>
2026-02-18 10:52:30 +0000

    single pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,33 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.${count.index}.0/24"
-  map_public_ip_on_launch = true
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
+resource "aws_security_group" "ecs_sg" {
+  name   = "ecs-strapi-sg"
+  vpc_id = data.aws_vpc.default.id
 
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
-}
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_route" "internet" {
-  route_table_id         = aws_route_table.rt.id
-  gateway_id             = aws_internet_gateway.gw.id
-  destination_cidr_block = "0.0.0.0/0"
-}
-
-resource "aws_route_table_association" "assoc" {
-  count          = 2
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.rt.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_ecs_cluster" "cluster" {
   name = "strapi-cluster-libin"
 }
-
-
