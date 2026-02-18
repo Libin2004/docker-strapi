1cf5434182383c08c03ac5ab1f78380f053ec186
Libin2004 <libinlibinlibin10@gmail.com>
2026-02-18 10:42:02 +0000

    single pipeline

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,38 +1,17 @@
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-
 resource "aws_ecs_task_definition" "strapi" {
   family                   = "strapi-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  execution_role_arn = var.ecs_task_role_arn
+  task_role_arn      = var.ecs_task_role_arn
 
   container_definitions = jsonencode([
     {
-      name      = "strapi"
-      image     = var.image
+      name  = "strapi"
+      image = var.image
       essential = true
 
       portMappings = [
@@ -41,44 +20,22 @@ resource "aws_ecs_task_definition" "strapi" {
           hostPort      = 1337
         }
       ]
-
-      environment = [
-        { name = "NODE_ENV", value = "production" },
-        { name = "APP_KEYS", value = "key1,key2,key3,key4" },
-        { name = "API_TOKEN_SALT", value = "random_salt_123" },
-        { name = "ADMIN_JWT_SECRET", value = "admin_secret_123" },
-        { name = "JWT_SECRET", value = "jwt_secret_123" },
-
-        { name = "DATABASE_CLIENT",   value = "postgres" },
-        { name = "DATABASE_HOST",     value = aws_db_instance.postgres.address },
-        { name = "DATABASE_PORT",     value = "5432" },
-        { name = "DATABASE_NAME",     value = var.db_name },
-        { name = "DATABASE_USERNAME", value = var.db_username },
-        { name = "DATABASE_PASSWORD", value = var.db_password }
-      ]
     }
   ])
 }
-
 
 resource "aws_ecs_service" "service" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.strapi.arn
-  launch_type     = "FARGATE"
   desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = aws_subnet.public[*].id
-    security_groups  = [aws_security_group.ecs_sg.id]
+    subnets         = data.aws_subnets.default.ids
+    security_groups = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
   }
-}
-Required variables
-Make sure you have:
 
-variables.tf
-variable "image" {}
-variable "db_name" {}
-variable "db_username" {}
-variable "db_password" {}
+  depends_on = [aws_ecs_task_definition.strapi]
+}
